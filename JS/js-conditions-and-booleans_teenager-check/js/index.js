0863c7b56da7bc8b7f824bfe01e8fcf8bf2eefe3
Kaya Chakmak <kycakmak@gmail.com>
2023-10-04 13:06:15 +0200

    if statement update

--- JS/js-conditions-and-booleans_teenager-check/js/index.js
+++ JS/js-conditions-and-booleans_teenager-check/js/index.js
@@ -1,13 +1,13 @@
 const input = document.querySelector('[data-js="age-input"]');
 const button = document.querySelector('[data-js="age-button"]');
-let output = document.querySelector('[data-js="age-output"]');
+const output = document.querySelector('[data-js="age-output"]');
 
 button.addEventListener("click", () => {
   // Number(x) converts the string from the input to a number
   // Beware: input values are *always* strings!
   const age = Number(input.value);
 
-  if (age >= 18) {
+  if (12 <= age >= 18) {
     output.textContent = "You are not a teen";
   } else {
     output.textContent = "You are a teen";
