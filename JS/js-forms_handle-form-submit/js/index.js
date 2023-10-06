4d1d8634343d23e8aca306336408f89bd12c1e93
Kaya Chakmak <kycakmak@gmail.com>
2023-10-06 15:34:02 +0200

    Handle form submit

--- JS/js-forms_handle-form-submit/js/index.js
+++ JS/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,12 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  event.preventDefault();
+  console.log(data);
+  event.target.reset();
+  event.target.elements.firstName.focus();
+});
