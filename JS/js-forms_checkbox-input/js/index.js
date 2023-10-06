2ab54cda2e0eae08ae451e3b71813b24ed17e5a7
Kaya Chakmak <kycakmak@gmail.com>
2023-10-06 16:57:42 +0200

    checkbox

--- JS/js-forms_checkbox-input/js/index.js
+++ JS/js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const success = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -16,9 +17,16 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
-
+  if (!tosCheckbox.checked) {
+    return;
+  }
   // --^-- write your code here --^--
 
-  // eslint-disable-next-line no-alert
+  // eslint-disable-next-line no-
   alert("Form submitted");
+  success.removeAttribute("hidden");
+});
+
+tosCheckbox.addEventListener("change", () => {
+  tosCheckbox.checked ? hideTosError() : showTosError();
 });
