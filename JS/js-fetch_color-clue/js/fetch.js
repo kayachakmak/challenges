6e2dad2d7c24b67946ae1fa64bca9de9c11b0da2
Kaya Chakmak <kycakmak@gmail.com>
2023-10-17 15:58:22 +0200

    add color-clue

--- JS/js-fetch_color-clue/js/fetch.js
+++ JS/js-fetch_color-clue/js/fetch.js
@@ -18,6 +18,10 @@ export async function fetchNewColor() {
    */
 
   // --v-- your code here --v--
-
+  const response = await fetch(colorApiUrl);
+  const data = await response.json();
+  setColorToGuess(data.hex.value, data.name.value);
   // --^-- your code here --^--
 }
+
+fetchNewColor();
