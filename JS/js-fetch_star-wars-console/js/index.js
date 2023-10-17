ebf2aadec9150c51a3480d3b7f379e88a67934f6
Kaya Chakmak <kycakmak@gmail.com>
2023-10-17 15:23:19 +0200

    star wars fetch

--- JS/js-fetch_star-wars-console/js/index.js
+++ JS/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,31 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+/* async function fetchData() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data[2]["eye-color"]);
+      console.log(data);
+    } else {
+      console.error("Bad response");
+    }
+  } catch (error) {
+    console.error("An error occured");
+  }
+}
+ */
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.results);
+    console.log("R2-2D's eye color: " + data.results[2]["eye_color"]);
+    console.log("Princess Leia's height: " + data.results[4].height);
+  } catch (error) {
+    console.log("An error occured", error);
+  }
+}
 
 fetchData();
