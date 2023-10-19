bce9b2446a74c5fc2be940c469f881df1e331280
Kaya Chakmak <kycakmak@gmail.com>
2023-10-19 10:51:15 +0200

    for great justice

--- JS/js-unit-testing_get-animal/index.js
+++ JS/js-unit-testing_get-animal/index.js
@@ -1,1 +1,9 @@
-export function getAnimal() {}
+export function getAnimal(name) {
+  if (name === "cats") {
+    return "I totally love cats!";
+  } else if (name === undefined) {
+    return "I do not like animals at all!";
+  } else {
+    return `I like ${name}!`;
+  }
+}
