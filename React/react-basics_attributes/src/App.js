1decbaba4e8ca3da486d20f956a9a074fadf77f0
Kaya Chakmak <kycakmak@gmail.com>
2023-10-24 11:39:06 +0200

    attributes

--- React/react-basics_attributes/src/App.js
+++ React/react-basics_attributes/src/App.js
@@ -2,5 +2,21 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return Article();
+}
+
+function Article() {
+  return (
+    <article>
+      <h2 className="article__title">Hello World</h2>
+      <label htmlFor="input">Please enter text</label>
+      <input type="text" name="input" id="input" />
+      <a
+        className="article__link"
+        href="https://developer.mozilla.org/en-US/docs/Web/Events#event_listing"
+      >
+        Here is our link
+      </a>
+    </article>
+  );
 }
