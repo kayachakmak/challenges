b944e01f13e43951514380298cc454a9cd18d42b
Kaya Chakmak <kycakmak@gmail.com>
2023-10-25 11:26:54 +0200

    add again

---
+++ React/react-nesting_navigation/src/components/Header.js
@@ -0,0 +1,30 @@
+import avatar from "./src/img/avatar.jpg";
+import logo from "./src/img/logo.jpg";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <a href="#">
+        <img className="round-image" src={`logo`} alt="logo" />
+      </a>
+      <nav>
+        <a className="navigation__link" href="#home">
+          Home
+        </a>
+        <a className="navigation__link" href="#about">
+          About
+        </a>
+        <a className="navigation__link" href="#impressum">
+          Impressum
+        </a>
+      </nav>
+      <button
+        type="button"
+        onClick={() => console.log("I could toggle a profile!")}
+        aria-label="toggle profile"
+      >
+        <img className="round-image" src={avatar} alt="avatar" />
+      </button>
+    </header>
+  );
+}
