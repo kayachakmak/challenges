07ce65c7032ba98078fb12375f704d1bfd8bc928
Kaya Chakmak <kycakmak@gmail.com>
2023-11-13 17:35:20 +0100

    only dimmed is missing

--- React/react-global-state_lights/pages/_app.js
+++ React/react-global-state_lights/pages/_app.js
@@ -1,11 +1,45 @@
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
+import { useState } from "react";
+
+const initial = [
+  { id: 1, name: "Living Room", isOn: false },
+  { id: 2, name: "Kitchen", isOn: false },
+  { id: 3, name: "Bedroom", isOn: false },
+  { id: 4, name: "Bathroom", isOn: false },
+  { id: 5, name: "Garage", isOn: false },
+  { id: 6, name: "Porch", isOn: false },
+];
 
 export default function App({ Component, pageProps }) {
+  const [lights, setLights] = useState(initial);
+
+  function handleToggle(id) {
+    setLights(
+      lights.map((light) => {
+        return light.id !== id ? light : { ...light, isOn: !light.isOn };
+      })
+    );
+  }
+
+  function allLightsOff() {
+    return setLights(lights.map((light) => ({ ...light, isOn: false })));
+  }
+
+  function allLightsOn() {
+    return setLights(lights.map((light) => ({ ...light, isOn: true })));
+  }
+
   return (
     <Layout>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component
+        {...pageProps}
+        lights={lights}
+        onToggle={handleToggle}
+        onOff={allLightsOff}
+        onOn={allLightsOn}
+      />
     </Layout>
   );
 }
