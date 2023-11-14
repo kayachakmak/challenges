ff4ab4dbd44e9d6b3f1f42b2e6c31642559dfbbc
Kaya Chakmak <kycakmak@gmail.com>
2023-11-14 12:01:24 +0100

    final

---
+++ React/react-data-fetching_star-wars/pages/characters/[id].js
@@ -0,0 +1,28 @@
+import { useRouter } from "next/router";
+import Card from "../../components/Card";
+import Layout from "../../components/Layout";
+import useSWR from "swr";
+
+export default function Character() {
+  const router = useRouter();
+  const { id } = router.query;
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    `https://swapi.dev/api/people/${id}`,
+    fetcher
+  );
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  return (
+    <Layout>
+      <Card
+        id={id}
+        name={data.name}
+        height={data.height}
+        eyeColor={data["eye_color"]}
+        birthYear={data["birth_year"]}
+      />
+    </Layout>
+  );
+}
