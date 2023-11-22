537d05f2f0abc6cd97d1268e9d4841f97be86bf0
Kaya Chakmak <kycakmak@gmail.com>
2023-11-22 16:21:13 +0100

    final

--- Backend/backend-read_products/pages/api/products/index.js
+++ Backend/backend-read_products/pages/api/products/index.js
@@ -2,11 +2,26 @@ import dbConnect from "@/db/connect";
 import Product from "@/db/models/Product";
 
 export default async function handler(request, response) {
-  await dbConnect();
-  const products = await Product.find();
-  if (request.method === "GET") {
-    console.log("data ", products);
-    return response.status(200).json(products);
+  try {
+    if (request.method === "GET") {
+      await dbConnect();
+      const products = await Product.find();
+      return response.status(200).json(products);
+    }
+    if (request.method === "POST") {
+      try {
+        const productData = request.body;
+        await Product.create(productData);
+
+        response.status(201).json({ status: "A new product added" });
+      } catch (error) {
+        console.log(error);
+        response.status(400).json({ error: error.message });
+      }
+
+      return response.status(200).json(products);
+    }
+  } catch (err) {
+    console.log(err);
   }
-  return response.status(405).json({ message: "Method not allowed" });
 }
